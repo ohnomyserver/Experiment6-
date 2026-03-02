1094f186635c32195f035a9a7835a0702d7d2428
ohnomyserver <harshagrawal0586@gmail.com>
2026-03-02 12:40:20 +0530

    Added resume PDF

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "<h1> Harsh Agrawal is a good boy. </h1>"
+def home():
+    return """
+    <h1>Harsh Agrawal</h1>
+    <a href="/static/Resume-HarshAgrawal.pdf" target="_blank">View My Resume</a>
+    """
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
+    app.run(host="0.0.0.0", port=5000)
